a9dd16d2ecb889a71aebedaa4e6c05f801ea685f
Aneesh Kher <aneeshkher@gmail.com>
2015-10-23 13:49:57 -0400

    Small changes to comment.js. Add comments to sample.js

--- comment.js
+++ comment.js
@@ -1,64 +1,70 @@
 var fs = require("fs");
-var fileName = "commenttest.js";
+var fileName = "./app/sample.js";
 var commentcount=0;
 var normalcount=0;
 
 fs.exists(fileName, function(exists) {
-  if (exists) {
-    fs.stat(fileName, function(error, stats) {
-      fs.open(fileName, "r", function(error, fd) {
-        var buffer = new Buffer(stats.size);
+ 	if (exists) {
+    	fs.stat(fileName, function(error, stats) {
+    	 	fs.open(fileName, "r", function(error, fd) {
+    	    	var buffer = new Buffer(stats.size);
 
-        fs.read(fd, buffer, 0, buffer.length, null, function(error, bytesRead, buffer) {
-          var data = buffer.toString("utf8", 0, buffer.length);
-          
+    	    	fs.read(fd, buffer, 0, buffer.length, null, function(error, bytesRead, buffer) {
+    	     		var data = buffer.toString("utf8", 0, buffer.length);
+    	      
 
-          var split=data.split('\n');
-          
-          for(var i=0;i<split.length;i++)
-          {
-          	if(split[i].indexOf('//')>-1)
-          	{
-          		commentcount++;
-          	}
-          /*	else
-          	{
-          		normalcount++;
-          	}*/
-          }
-          var temp;
-          for(var i=0;i<split.length;i++)
-          {
-  	    	if(split[i].indexOf("/*")>-1)
-          	{
-          		//temp=1;
+    	      		var split=data.split('\n');
+    	      
+    	      		for(var i=0;i<split.length;i++)
+    	      		{
+    	      			if(split[i].indexOf('//')>-1)
+    	      			{
+    	      				commentcount++;
+    	      			}
+    	      			/*	else
+    	      			{
+    	      				normalcount++;
+    	      			}*/
+    	      		}
+    	      		var temp;
+    	      		for(var i=0;i<split.length;i++)
+    	      		{
+  		    			if(split[i].indexOf("/*")>-1)
+    	      			{
+    	      				//temp=1;
 
-          		commentcount++;
-          		if(split[i].indexOf("*/")==-1)
-          		{
-          			var j=i+1;
-          			while(split[j].indexOf("*/")==-1)
-          			{
-          				commentcount++;
-          				j++;
-          			}
-          			commentcount++;
-          		}
-          	}
-          }
+    	      				commentcount++;
+    	      				if(split[i].indexOf("*/")==-1)
+    	      				{
+    	      					var j=i+1;
+    	      					while(split[j].indexOf("*/")==-1)
+    	      					{
+    	      						commentcount++;
+    	      						j++;
+    	      					}
+    	      					commentcount++;
+    	      				}
+    	      			}
+    	      		}
 
-         
-         normalcount=split.length-commentcount;
-         var ratio=commentcount/normalcount;
-         
-          if(ratio<0.1||ratio>0.5)
-          {
-          	
-          	process.exit(1);
-          }
-          fs.close(fd);;
-        });
-      });
-    });
-  }
+    	     
+    	     		normalcount=split.length-commentcount - 1;
+    	     		var ratio=commentcount/split.length;
+
+					console.log("Comment count: ", commentcount);
+					console.log("Lines of code: ", normalcount);
+					console.log("Number of lines in the file", split.length - 1);
+					console.log("Ratio: ", ratio);
+			
+    	     
+    	      		if(ratio<0.1||ratio>0.5)
+    	      		{
+    	      	
+    	      			process.exit(1);
+    	      		}
+    	      		fs.close(fd);;
+    	    	});
+    	  	});
+    	});
+	}
 });
