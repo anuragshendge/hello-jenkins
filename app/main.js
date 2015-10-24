996019dd3274f11235e5027fd2bd747a499637c6
ashendg <ashendg@ncsu.edu>
2015-10-23 20:32:49 -0400

    Add coverage LESS

--- app/main.js
+++ app/main.js
@@ -209,7 +209,8 @@ function constraints(filePath)
 			traverse(node, function(child)
 			{
 				
-				if( child.type === 'BinaryExpression' && (child.operator == "==" || child.operator == "===") )
+				if( child.type === 'BinaryExpression' && (child.operator == "==" ))
+			//		|| child.operator == "===") )
 				{
 					if( child.left.type == 'Identifier' && params.indexOf( child.left.name ) > -1)
 					{
